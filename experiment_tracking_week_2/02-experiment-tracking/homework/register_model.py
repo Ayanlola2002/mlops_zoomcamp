0c570c5ae0d1009fed7831e79d1be7e036cc8926
ayanlola <ayanlola2002@yahoo.com>
2022-05-30 12:13:03 +0000

    best commit

--- experiment_tracking_week_2/02-experiment-tracking/homework/register_model.py
+++ experiment_tracking_week_2/02-experiment-tracking/homework/register_model.py
@@ -41,7 +41,7 @@ def train_and_log_model(data_path, params):
         rf = RandomForestRegressor(**params)
         rf.fit(X_train, y_train)
 
-        # evaluate model on the validation and test sets
+        # model validation tracking
         valid_rmse = mean_squared_error(y_valid, rf.predict(X_valid), squared=False)
         mlflow.log_metric("valid_rmse", valid_rmse)
         test_rmse = mean_squared_error(y_test, rf.predict(X_test), squared=False)
@@ -52,7 +52,7 @@ def run(data_path, log_top):
 
     client = MlflowClient()
 
-    # retrieve the top_n model runs and log the models to MLflow
+    # extracking the top_n model runs and log the models to MLflow
     experiment = client.get_experiment_by_name(HPO_EXPERIMENT_NAME)
     runs = client.search_runs(
         experiment_ids=experiment.experiment_id,
@@ -65,10 +65,17 @@ def run(data_path, log_top):
 
     # select the model with the lowest test RMSE
     experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
-    # best_run = client.search_runs( ...  )[0]
+    best_run = client.search_runs(experiment_ids = 3,
+                                  run_view_type= mlflow.entities.ViewType.ACTIVE_ONLY,
+                                  max_results=1,  
+                                  order_by=["metrics.test_rmse ASC"]
+                                 )
+    
+    best_run_id = best_run[0].info.run_id
 
     # register the best model
-    # mlflow.register_model( ... )
+    mlflow.register_model(model_uri = f"runs:/{best_run_id}/model", 
+                          name  = "Best_RandomForestRegressor_BasedOn_202103_testdata")
 
 
 if __name__ == '__main__':
--- experiment_tracking_week_2/02-experiment-tracking/homework/train.py
+++ experiment_tracking_week_2/02-experiment-tracking/homework/train.py
@@ -7,29 +7,30 @@ from sklearn.metrics import mean_squared_error
 
 import mlflow
 
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
-mlflow.set_experiment("green-nyc-taxi-experiment")
+
 
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
         return pickle.load(f_in)
 
-mlflow.autolog()
+
 def run(data_path):
-    with mlflow.start_run():
-        mlflow.set_tag("model","Random_forest")
-        
+    mlflow.set_tracking_uri('sqlite:///mlflow.db')
+    mlflow.set_experiment('nyc-taxi-experiment')
+
+    # enable autologging
+    mlflow.sklearn.autolog()
+
+    with  mlflow.start_run() as run:
         X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
         X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
-        params = {"max_depth": 3, "random_state": 42}
-        rf = RandomForestRegressor(**params)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_valid)
+
         rmse = mean_squared_error(y_valid, y_pred, squared=False)
-        mlflow.log_params(params)
-        mlflow.sklearn.log_model(rf, artifact_path="sklearn-model")
 
 
 
