a74d19eac8196f0b711afe3c4d58bea9c723f734
ayanlola2002 <ayanlola2002@yahoo.com>
2022-06-13 13:18:09 +0000

    change

--- workflow_orchestration_week_3/03-orchestration/notebooks/deployment.py
+++ workflow_orchestration_week_3/03-orchestration/notebooks/deployment.py
@@ -3,8 +3,8 @@ from prefect.orion.schemas.schedules import CronSchedule
 
 
 DeploymentSpec(
-    name="cron-schedule-deployment-daniel",
+    name="cron-schedule-deployment-ayanlola",
     flow_location="/home/ayanlola/my_mlops_zoomcamp/workflow_orchestration_week_3",
     schedule=CronSchedule(cron="0 9 15 * *"),
-    tags=['flow_test_tuna_cron']
+    tags=['flow_test_ayanlola_cron']
 )
