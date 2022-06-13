7e453c93d13d52353ebd70184f49b2a9c2d40ab0
ayanlola2002 <ayanlola2002@yahoo.com>
2022-06-13 21:14:46 +0000

    ch2

--- workflow_orchestration_week_3/03-orchestration/deployment.py
+++ workflow_orchestration_week_3/03-orchestration/deployment.py
@@ -6,7 +6,7 @@ from prefect.flow_runners import SubprocessFlowRunner
 DeploymentSpec(
     name="cron-schedule-deployment-ayanlola",
     flow_runner=SubprocessFlowRunner(),
-    flow_location="/home/ayanlola/my_mlops_zoomcamp/workflow_orchestration_week_3",
+    flow_location="/home/ayanlola/my_mlops_zoomcamp/workflow_orchestration_week_3/03-orchestration/homework.py",
     schedule=CronSchedule(cron="0 9 15 * *"),
     tags=['flow_test_ayanlola_cron']
 )
