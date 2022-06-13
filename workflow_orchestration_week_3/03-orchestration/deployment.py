05976d749fa8b0a589727df005b202fe1a030f93
ayanlola2002 <ayanlola2002@yahoo.com>
2022-06-13 20:16:18 +0000

    ch11

---
+++ workflow_orchestration_week_3/03-orchestration/deployment.py
@@ -0,0 +1,12 @@
+from prefect.deployments import DeploymentSpec
+from prefect.orion.schemas.schedules import CronSchedule
+from prefect.flow_runners import SubprocessFlowRunner
+
+
+DeploymentSpec(
+    name="cron-schedule-deployment-ayanlola",
+    flow_runner=SubprocessFlowRunner(),
+    flow_location="/home/ayanlola/my_mlops_zoomcamp/workflow_orchestration_week_3",
+    schedule=CronSchedule(cron="0 9 15 * *"),
+    tags=['flow_test_ayanlola_cron']
+)
